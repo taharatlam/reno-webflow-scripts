a57ed8e9eb558c7ff46158635f53e395303c0597
Taha Ratlam Wala <56840043+taharatlam@users.noreply.github.com>
2025-12-09 22:15:49 +0530

    ss

--- career.js
+++ career.js
@@ -35,7 +35,7 @@ function CareerSecAnimation() {
           ease: "power3.out",
         }).to(word, {
           y: -lineHeight,
-          //   opacity: 0,
+          opacity: 0,
           duration: 0.6,
           ease: "power3.in",
           delay: 1, // visible pause
